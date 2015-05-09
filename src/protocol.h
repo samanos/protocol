f4f136517df8b05593bfbd7f53f93518152182f6
Martynas Mickevičius <mmartynas@gmail.com>
2015-05-09 15:01:00 +0300

    initial protocol

---
+++ src/protocol.h
@@ -0,0 +1,17 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#define VENDOR_ID                 0x16c0
+#define VENDOR_NAME               "github.com/samanos"
+#define VENDOR_NAME_ARRAY         'g', 'i', 't', 'h', 'u', 'b', '.', 'c', 'o', 'm', '/', 's', 'a', 'm', 'a', 'n', 'o', 's'
+#define VENDOR_NAME_ARRAY_LEN     18
+
+#define PRODUCT_ID                0x05dc
+#define PRODUCT_NAME              "tiny-bridge"
+#define PRODUCT_NAME_ARRAY        't', 'i', 'n', 'y', '-', 'b', 'r', 'i', 'd', 'g', 'e'
+#define PRODUCT_NAME_ARRAY_LEN    11
+
+#define USB_NRF_STATUS    2
+#define USB_NRF_TRANSFER1 3
+
+#endif
